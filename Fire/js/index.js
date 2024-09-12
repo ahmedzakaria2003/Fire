5e1682655032a118822db9243486b60df561a64e
ahmedzakaria2003 <ahmedmohamedzakaria423@gmail.com>
2024-09-12 17:13:18 +0300

    Delete Fire directory

--- Fire/js/index.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("mousemove", console.log("hi"))
-
-var img = document.querySelector("img");
-
-document.addEventListener("mousemove", function(e){
-    console.log("hi", e.clientY);
-    img.style.top=`${e.clientY}px`;
-    img.style.left= `${e.clientX}px`;
-}) 
